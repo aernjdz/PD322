278ff578e3f43cb1c0502acba478720f2cebddad
z4developer <zarichniukandriy5@gmail.com>
2023-07-07 15:53:31 +0300

    Happy New Year 2023 😒

--- homework/python/02_matrix_hw.py
+++ homework/python/02_matrix_hw.py
@@ -56,9 +56,35 @@ from random import randint
 
 # №5
 
+# №7
+# officer_position = input("Enter the position of the Officer :: ")
+# column = ord(officer_position[0]) - ord('a')
+# row = int(officer_position[1]) - 1
+def відмітити_клітинки_під_загрозою_тури(позиція):
+    стовпець = ord(позиція[0]) - ord('a')
+    рядок = int(позиція[1]) - 1
 
+    дошка = [['.' for _ in range(8)] for _ in range(8)]
 
+    # Відмітити клітинки в одному рядку
+    for i in range(8):
+        дошка[рядок][i] = '*'
 
+    # Відмітити клітинки в одному стовпці
+    for i in range(8):
+        дошка[i][стовпець] = '*'
+
+    # Позицію тури позначити як 'T'
+    дошка[рядок][стовпець] = 'T'
+
+    # Вивести шахову дошку
+    print("  a b c d e f g h")
+    for i in range(8):
+        рядок_вивід = ' '.join(дошка[i])
+        print(f"{i + 1} {рядок_вивід}")
+
+позиція_тури = 'f6'
+відмітити_клітинки_під_загрозою_тури(позиція_тури)
 horse_position = input("Enter the position of the horse :: ")
 column = ord(horse_position[0]) - ord('a')
 row = int(horse_position[1]) - 1
@@ -70,7 +96,7 @@ for walk in horse_walk:
     new_row = row + walk[0]
     new_column = column + walk[1]
     if 0 <= new_row < 8 and 0 <= new_column < 8:
-        board[new_row][new_column] = 'X'
+        board[new_row][new_column] = '*'
 
 print("  a b c d e f g h")
 for i, row in enumerate(board):
