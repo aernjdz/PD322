fd97bd0f0a68ca8a0be6ba6c8308e0dfef756bbf
z4developer <zarichniukandriy5@gmail.com>
2023-07-07 10:55:03 +0300

    Happy New Year 2023 😒

--- homework/python/02_matrix_hw.py
+++ homework/python/02_matrix_hw.py
@@ -1,4 +1,17 @@
 from random import randint
+# №1
 
-size = int(input("Enter list size :: "))
 list = [randint(5,30) for i in range(20)]
+
+print(f"List :: {' '.join(map(str,list))}")
+
+max_index = list.index(max(list))
+min_index = list.index(min(list))
+
+start_index = min(min_index,max_index) + 1
+end_index = max(min_index,max_index)
+
+for i in range(start_index,end_index):
+    list[i] *= 2
+
+print(f"Result :: {' '.join(map(str,list))}")
