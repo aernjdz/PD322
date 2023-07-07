eebf6a8b616dbe90c17738fb9921ec9235b96c91
z4developer <zarichniukandriy5@gmail.com>
2023-07-07 13:31:04 +0300

    Happy New Year 2023 😒

--- homework/python/02_matrix_hw.py
+++ homework/python/02_matrix_hw.py
@@ -1,7 +1,7 @@
 from random import randint
 # №1
-
-list = [randint(5,30) for i in range(20)]
+size = int(input("Enter list size :: "))
+list = [randint(0,20) for i in range(20)]
 
 print(f"Before :: {' '.join(map(str,list))}")
 
@@ -17,7 +17,8 @@ for i in range(start_index,end_index):
 print(f"After  :: {' '.join(map(str,list))}")
 
 # №2
-list = [randint(5,30) for i in range(20)]
+size = int(input("Enter list size :: "))
+list = [randint(0,20) for i in range(20)]
 
 print(f"Before :: {' '.join(map(str,list))}")
 for i in range(0,len(list)-1,2):
@@ -25,8 +26,8 @@ for i in range(0,len(list)-1,2):
 print(f"After  :: {' '.join(map(str,list))}")
 
 # №3
-
-list = [randint(5,30) for i in range(20)]
+size = int(input("Enter list size :: "))
+list = [randint(0,20) for i in range(20)]
 print(f"List :: {' '.join(map(str,list))}")
 repeat = []
 count = {} 
@@ -34,3 +35,22 @@ for i in list:
     if list.count(i) > 1 and not i in repeat:
         repeat.append(i)
 print(f"Result :: {' '.join(map(str,repeat))}")
+
+# №4
+width,height = map(int,input("Enter matrix size  in format 1x1 (wxh):: ").split('x'))
+matrix = [[randint(0,20) for _ in range(width)] for i in range(height)]
+pading = len(str(sum(_ for rows in matrix for _ in rows)))
+row = ""
+for _ in matrix:
+    for num in _:
+         row += f"{num:<{pading}d}"
+    row += f" |{sum(_):<{pading}d}"
+    print(row)
+    row = ""
+column = ''
+for i in range(width):
+   column += f"{sum(rows[i] for rows in matrix):<{pading}d}"
+column += f" |{sum(_ for rows in matrix for _ in rows):<{pading}d}"
+print("-"*len(column))
+print(column)
+
