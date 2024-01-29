e6da9b63a00599caa4ead162cdbecae5634a5a7d
Z4_DEV <80629311+z4developer@users.noreply.github.com>
2024-01-29 20:13:29 +0200

    Easy 🤔🕛

---
+++ C#/Homework_13/Homework_13/Currency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_13
+{
+    class Currency
+    {
+        public string ccy { get; set; }
+        public string base_ccy { get; set; }
+        public decimal buy { get; set; }
+        public decimal sale { get; set; }
+    }
+}
---
+++ C#/Homework_13/Homework_13/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Homework_13
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+             
+                WebClient wc = new WebClient();
+                string json = wc.DownloadString("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5");
+
+              
+                List<Currency> currencies = JsonConvert.DeserializeObject<List<Currency>>(json);
+               
+           
+                Console.WriteLine("Available currencies:");
+                foreach (var currency in currencies)
+                {      
+
+                    Console.WriteLine($"{currency.ccy} - {currency.base_ccy}");
+                }
+
+                Console.Write("Enter the currency code (e.g., USD): ");
+                string selectedCurrencyCode = Console.ReadLine().ToUpper();
+
+             
+                Currency selectedCurrency = currencies.Find(c => c.ccy == selectedCurrencyCode);
+
+           
+                if (selectedCurrency != null)
+                {
+                    Console.WriteLine($"Buy rate for {selectedCurrency.ccy}: {selectedCurrency.buy}");
+                    Console.WriteLine($"Sale rate for {selectedCurrency.ccy}: {selectedCurrency.sale}");
+                }
+                else
+                {
+                    Console.WriteLine("Currency not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
