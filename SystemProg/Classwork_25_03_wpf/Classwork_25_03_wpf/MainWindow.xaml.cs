210512b11676c140c551a86d28a5c34e8d1069b7
Z4_DEV <80629311+z4developer@users.noreply.github.com>
2024-03-25 16:57:33 +0200

    Easy 🕛🤔

---
+++ SystemProg/Classwork_25_03_wpf/Classwork_25_03_wpf/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Classwork_25_03_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+      private void LaunchProgram(object sender, RoutedEventArgs e)
+        {
+            Button btn =sender as Button;
+            if (btn != null )
+            {
+                string cmd = btn.Tag.ToString();
+                Launch(cmd);
+            }
+        }
+
+        private void Launch(string programName)
+        {
+            try
+            {
+                Process.Start(programName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error launching {programName}: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void LaunchCustomProgram(object sender, RoutedEventArgs e)
+        {
+            string customProgramPath = CustomProgramPathTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(customProgramPath))
+            {
+                Launch(customProgramPath);
+            }
+            else
+            {
+                MessageBox.Show("Please specify the path of the custom program.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        /*        private void Refresh(object sender, RoutedEventArgs e)
+                {
+                    grid.ItemsSource = Process.GetProcesses();
+
+                }*/
+    }
+}
---
+++ SystemProg/classwork_25_03/classwork_25_03/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Diagnostics;
+using System.Net.Security;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*  Process current = Process.GetCurrentProcess();
+          current.PriorityClass = ProcessPriorityClass.High;
+          Console.WriteLine("-------Current Process info------");
+          Console.WriteLine($"PriorityClass :: {current.BasePriority}");
+          Console.WriteLine($"ProcessName :: {current.ProcessName}");
+          Console.WriteLine($"Id :: {current.Id}");
+          Console.WriteLine($"MachineName :: {current.MachineName}");
+          Console.WriteLine($"StartTime :: {current.StartTime}");
+          Console.WriteLine($"TotalProcessorTime :: {current.TotalProcessorTime}");
+          Console.WriteLine($"--------------------------------");
+          Console.ReadKey();*/
+
+        /*  Process[] processes = Process.GetProcesses();
+          foreach (Process p in processes)
+          {
+              try
+              {
+                  Console.WriteLine($"{p.ProcessName,-40} {p.Id,-10} {p.PriorityClass , -20} {p.StartTime}");
+              }catch (Exception ex)
+              {
+                 Console.ForegroundColor = ConsoleColor.Red;
+                  Console.WriteLine($"Error with {p.ProcessName,-30} : {ex.Message}");
+
+                  Console.ResetColor();
+
+              }
+          }
+          Console.ReadKey();*/
+
+        /* Process.Start("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe", "google.com");*/
+
+       
+
+    }
+}
