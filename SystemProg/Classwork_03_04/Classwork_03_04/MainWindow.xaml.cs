a879e081049f7132aecdad2cf53841c0b9fb7736
Z4_DEV <80629311+z4developer@users.noreply.github.com>
2024-04-03 16:12:59 +0300

    Easy 🕛🤔

---
+++ SystemProg/Classwork_03_04/Classwork_03_04/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Classwork_03_04
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ShowRandomNumber(object sender, RoutedEventArgs e)
+        {
+            /*  int value = GenerateValue();
+              List.Items.Add(value);*/
+
+            /*  Task<int> task = Task.Run(GenerateValue);
+              int value = await task; 
+
+              List.Items.Add(value);*/
+            int value;
+            if (int.TryParse(number.Text, out value))
+            {
+               // List.Items.Add(await GenerateValueAsync(value));
+                List.Items.Add(await CalculateFactorialAsync(value));
+            }
+            else
+            {
+                MessageBox.Show("Error", "error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        /* int GenerateValue() 
+         { 
+          Thread.Sleep(rnd.Next(10000));
+             return rnd.Next(1000);
+         }*/
+        Task<int> GenerateValueAsync(int value)
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(rnd.Next(10000));
+                return rnd.Next(value);
+            });
+
+        }
+        async Task<long> CalculateFactorialAsync(int n)
+        {
+            return await Task.Run(() =>
+            {
+                if (n < 0)
+                {
+                    MessageBox.Show("Error", "Factorial is not defined for negative numbers.", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                long result = 1;
+                for (int i = 2; i <= n; i++)
+                {
+                    result *= i;
+                }
+                Thread.Sleep(rnd.Next(10000));
+                return result;
+            });
+        }
+    }
+}
