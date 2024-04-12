100ec50486183ada41eafa275a8568bff78a0b3a
Z4_DEV <80629311+z4developer@users.noreply.github.com>
2024-04-13 00:02:57 +0300

    Easy 🕛🤔

--- SystemProg/Exam_wpf/Exam_wpf/obj/Debug/net8.0-windows/Exam_wpf.AssemblyInfo.cs
+++ SystemProg/Exam_wpf/Exam_wpf/obj/Debug/net8.0-windows/Exam_wpf.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exam_wpf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0104c8775560fb7bdae824bb39387f4b364f17dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+161a4e39f9003e2546b9f8b476536258cee14ff1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exam_wpf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exam_wpf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ SystemProg/Homework_06/Homework_06/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using PropertyChanged;
+
+namespace Homework_06
+{
+    public partial class MainWindow : Window
+    {
+        ViewModel model;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            model = new ViewModel();
+            this.DataContext = model;
+        }
+
+        private void OpenSource_btn(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = false;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                model.SourcePath = dialog.FileName;
+                srcTextBox.Text = model.SourcePath;
+            }
+        }
+
+        private async void Factorial_btn(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(model.SourcePath))
+            {
+                MessageBox.Show("Please select a valid source folder.");
+                return;
+
+            }
+            model.ResetProperties();
+
+            try
+            {
+                string[] lines = await Task.Run(() => File.ReadAllLines(model.SourcePath));
+
+             
+                int totalNumbers = 0;
+                foreach (string line in lines)
+                {
+                    totalNumbers += line.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+                }
+                model.TotalNumbers = totalNumbers;
+
+                await Task.Run(() =>
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] numbers = line.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        Parallel.ForEach(numbers, num =>
+                        {
+                            if (int.TryParse(num, out int number))
+                            {
+                             
+                                long factorial = CalculateFactorial(number);
+                             
+
+                                Factorials info = new Factorials(number);
+                                info.Factorial = factorial;
+
+                                Application.Current.Dispatcher.Invoke(() =>
+                                {
+                                    model.AddProcess(info);
+                                    model.UpdatePercentage();
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private long CalculateFactorial(int number)
+        {
+            long result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModel 
+    {
+        private ObservableCollection<Factorials> processes;
+        private int totalNumbers = 0;
+
+        public string SourcePath { get; set; }
+      
+        public double Percentage { get; set; }
+        public int ProgressPercentageInt => (int)Percentage;
+        public int TotalNumbers
+        {
+            get { return totalNumbers; }
+            set
+            {
+                totalNumbers = value;
+
+            }
+        } 
+
+        public ViewModel()
+        {
+            processes = new ObservableCollection<Factorials>();
+        }
+
+        public void ResetProperties()
+        {
+         
+            Percentage = 0;
+            TotalNumbers = 0;
+            processes.Clear();
+        }
+        public IEnumerable<Factorials> Processes => processes;
+
+        public void AddProcess(Factorials info)
+        {
+            processes.Add(info);
+        }
+
+        public void UpdatePercentage()
+        {
+            if (processes.Count == 0)
+            {
+                Percentage = 0;
+            }
+            else
+            {     
+                Percentage = (double)processes.Count / TotalNumbers * 100;
+            }
+        }
+    }
+
+    [AddINotifyPropertyChangedInterface]
+    public class Factorials
+    {
+        public int Num { get; set; }
+        public long Factorial { get; set; }
+
+        public Factorials(int num)
+        {
+            this.Num = num;
+        }
+    }
+}
