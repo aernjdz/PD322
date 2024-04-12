161a4e39f9003e2546b9f8b476536258cee14ff1
Z4_DEV <80629311+z4developer@users.noreply.github.com>
2024-04-12 22:17:56 +0300

    Easy 🕛🤔

--- SystemProg/Homework_04/Homework_04/MainWindow.xaml.cs
+++ SystemProg/Homework_04/Homework_04/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,7 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Microsoft.Win32;
 namespace Homework_04
 {
     /// <summary>
@@ -21,24 +22,60 @@ namespace Homework_04
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void AnalyzeButton_Click(object sender, RoutedEventArgs e)
         {
+            string text = inputTextBox.Text;
 
+            Task<string> analysisTask = Task.Run(() => AnalyzeText(text));
+
+            string report = await analysisTask;
+
+            if (showOnScreenRadioButton.IsChecked == true)
+            {
+                resultTextBox.Text = report;
+            }
+            else if (saveToFileRadioButton.IsChecked == true)
+            {
+                SaveReportToFile(report);
+                MessageBox.Show("Report saved to file.", "Report Saved");
+            }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private string AnalyzeText(string text)
         {
+            int interrogativeSentencesCount = text.Split('?').Length - 1;
+            int exclamatorySentencesCount = text.Split('!').Length - 1;
 
+            
+            string[] separators = new string[] { ".", "!", "?" };
+            string[] sentences = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            int sentencesCount = sentences.Length;
+
+            string[] words = text.Split(new char[] { ' ', ',', ':', ';', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+            int wordsCount = words.Length;
+            int charactersCount = text.Count(c => !char.IsWhiteSpace(c));
+
+            string report = $"Number of sentences: {sentencesCount}\n";
+            report += $"Number of words: {wordsCount}\n";
+            report += $"Number of characters: {charactersCount}\n";
+            report += $"Number of interrogative sentences: {interrogativeSentencesCount}\n";
+            report += $"Number of exclamatory sentences: {exclamatorySentencesCount}";
+
+            return report;
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+
+        private void SaveReportToFile(string report)
         {
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    File.WriteAllText(saveFileDialog.FileName, report);
+                }
+           
         }
     }
 }
